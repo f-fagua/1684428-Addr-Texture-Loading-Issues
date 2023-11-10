c6ad6e9a0a423b106e32f38b261bac967cc6800c
f-fagua <91572042+f-fagua@users.noreply.github.com>
2023-11-10 08:17:21 -0500

    testing the edited version of the customer (it works as expected)

--- Assets/Loader.cs
+++ Assets/Loader.cs
@@ -2,16 +2,28 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.AddressableAssets;
 using UnityEngine.ResourceManagement.AsyncOperations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.ResourceManagement.ResourceLocations;
+using UnityEngine.AddressableAssets.ResourceLocators;
+using System.Threading.Tasks;
+using System;
+using System.IO;
 
 public class Loader : MonoBehaviour
 {
-    public string addressableKey;
-    public Texture townhallEnvironment;
-    public bool readyToUse;
-
     public static Loader instance;
 
-    
+    public string addressableKey;
+    public string addressableKey2;
+
+    public Texture townhallEnvironment;
+    public Sprite iconUI;
+
+    public bool readyToUse;
+
+    [SerializeField] private AssetLabelReference labelReference;
+    [SerializeField] private AsyncOperationHandle downloadHandle;
 
     private void Awake()
     {
@@ -23,27 +35,83 @@ public class Loader : MonoBehaviour
         }
     }
 
-    async void Start()
+    private void Start()
     {
-        AsyncOperationHandle<Texture> handle = Addressables.LoadAssetAsync<Texture>(addressableKey);
-        await handle.Task;
+        Addressables.InitializeAsync();
+        DownloadAssetsAsync(labelReference);
+    }
+
+    private async void DownloadAssetsAsync(AssetLabelReference key)
+    {
+        downloadHandle = Addressables.DownloadDependenciesAsync(key);
+
+        try
+        {
+            while (!downloadHandle.IsDone)
+            {
+                Debug.Log("Asset Downloading");
+                await Task.Yield();
+            }
+        }
+        catch (Exception ex)
+        {
+            Debug.Log("Asset error: " + ex.Message);
+        }
+
+        Debug.Log("Asset Done");
+        StartCoroutine(LoadDataAsync());
+    }
+
+    IEnumerator LoadDataAsync()
+    {
+        AsyncOperationHandle<IList<IResourceLocation>> locationHandle = Addressables.LoadResourceLocationsAsync(addressableKey);
+        yield return locationHandle;
+
+        Debug.Log("Asset: " + locationHandle.Result[0]);
+
+        // ==================
+        AsyncOperationHandle<Texture> handle = Addressables.LoadAssetAsync<Texture>(locationHandle.Result[0]);
+        yield return handle;
 
         if (handle.Status == AsyncOperationStatus.Succeeded)
         {
             Debug.Log("Background Data Success to Download : " + handle.Result);
             Texture data = handle.Result;
-            Debug.Log("Texture : " + data);
             townhallEnvironment = data;
             readyToUse = true;
-            
         }
         else
         {
-            // Ada kesalahan dalam memuat sumber daya
-            Debug.Log("Background Data Failed to Download : " + addressableKey);
+            Debug.Log("Background Data Failed to Download : " + handle.Result);
+            Debug.Log("Background Data Failed to Download : " + handle.Status);
             readyToUse = false;
         }
 
-        Addressables.Release(handle);
+        //Addressables.Release(handle);
+
+        // ==================
+
+        //AsyncOperationHandle<Sprite> handle2 = Addressables.LoadAssetAsync<Sprite>(handle.Result[1]);
+        //await handle2.Task;
+
+        //if (handle2.Status == AsyncOperationStatus.Succeeded)
+        //{
+        //    Debug.Log("Background Data Success to Download : " + handle2.Result);
+
+        //    Sprite data = handle2.Result;
+        //    iconUI = data;
+        //    readyToUse = true;
+        //}
+        //else
+        //{
+        //    // Ada kesalahan dalam memuat sumber daya
+        //    Debug.Log("Background Data Failed to Download : " + handle2.Result);
+        //    Debug.Log("Background Data Failed to Download : " + handle2.Status);
+
+        //    readyToUse = false;
+        //}
+
+        //Addressables.Release(handle2);
+        // ==================s
     }
 }
